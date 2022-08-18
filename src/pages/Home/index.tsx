d7e0e7de5a5846a8c1768b89febdf7f471e8b24a
rbpolim <rodrigobpolim@gmail.com>
2022-08-17 21:39:27 -0300

    feat: improve Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,16 @@
+import { Footer } from "../../components/Footer";
 import { HeroSection } from "../../components/HeroSection";
 import { PostsSection } from "../../components/PostsSection";
 
 export function Home() {
   return (
-    <div className="bg-[##EFE6DD]">
+    <div className="bg-[##EFE6DD] text-slate-800">
       <div className="font-poppins p-10 max-w-5xl mx-auto">
         <HeroSection />
         <PostsSection />
       </div>
+
+      <Footer />
     </div>
   )
 }
