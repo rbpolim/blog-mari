1c6a64b0c988bbb3d958ace218015ddd2b3ead2c
rbpolim <rodrigobpolim@gmail.com>
2022-08-17 21:38:45 -0300

    feat: improve PostCard & PostsSection component

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom";
 
 import { Post } from "../../graphql/queries/get-posts-query";
 
+const badges = [
+  'asian',
+  'bakery',
+  'italian'
+]
 type PostCardProps = {
   post: Post;
 }
@@ -10,15 +15,24 @@ export function PostCard({ post }: PostCardProps) {
   return (
     <Link
       to={`/post/${post.slug}`}
-      className="p-6 border border-transparent hover:border hover:border-yellow-300 rounded-md transition-colors duration-200 hover:translate-y-1"
+      className="flex flex-col p-6 border border-zinc-800 hover:bg-gradient-to-b hover:from-transparent hover:to-orange-100
+        hover:border-orange-900 hover:shadow-lg rounded-md transition-colors duration-200 hover:translate-y-1 space-y-2"
     >
       <h3 className="text-left text-4xl font-bold">
         {post.title}
       </h3>
 
-      <p className="mt-4 text-left">
+      <p className="mt-10 text-left text-sm">
         {post.shortDescription}
       </p>
+
+      <div className="flex items-center gap-2 pt-4">
+        {badges.map((badge) => (
+          <span className="py-1 px-2 bg-white border border-orange-900 rounded-2xl text-sm text-orange-900">
+            {badge}
+          </span>
+        ))}
+      </div>
     </Link>
   )
 }
--- src/components/PostsSection/index.tsx
+++ src/components/PostsSection/index.tsx
@@ -16,16 +16,16 @@ export function PostsSection() {
   }
 
   return (
-    <section className="mt-20 space-y-10">
+    <section className="mt-20 space-y-10 my-32 ">
       <div className="relative mb-32">
         <h2 className="text-6xl font-bold">
           New Post
         </h2>
 
-        <div className="absolute bottom-0 -z-10 h-7 w-[320px] bg-yellow-300" />
+        <div className="absolute bottom-0 -z-10 h-7 w-[320px] bg-orange-100" />
       </div>
 
-      <div className="grid grid-cols-2 gap-20 ">
+      <div className="grid md:grid-cols-2 gap-20 ">
         {data?.posts.map((post) => (
           <PostCard
             key={post.id}
