8844fc69868229f46679caa4ed2ea8377af46de3
rbpolim <rodrigobpolim@gmail.com>
2022-08-21 20:57:39 -0300

    feat: new ShimmerCard component

--- src/components/ShimmerCard/index.tsx
+++ src/components/ShimmerCard/index.tsx
@@ -1,5 +1,10 @@
 export function ShimmerCard() {
   return (
-    <div className="w-96 h-40 bg-gradient-to-b from-orange-200 to-orange-300 rounded-md animate-pulse shadow-lg" />
+    <>
+      <div className="w-96 h-40 bg-gradient-to-b from-orange-200 to-orange-300 rounded-md animate-pulse shadow-lg" />
+      <div className="w-96 h-40 bg-gradient-to-b from-orange-200 to-orange-300 rounded-md animate-pulse shadow-lg" />
+      <div className="w-96 h-40 bg-gradient-to-b from-orange-200 to-orange-300 rounded-md animate-pulse shadow-lg" />
+      <div className="w-96 h-40 bg-gradient-to-b from-orange-200 to-orange-300 rounded-md animate-pulse shadow-lg" />
+    </>
   )
 }
